bef241e20be9d9a372cfa25c6ce6215f108f0633
Jak <deepanger@qq.com>
2018-09-05 22:36:19 +0800

    add files

--- binary_search.cpp
+++ binary_search.cpp
@@ -44,12 +44,41 @@ int binary_search(int* a, int n, int x)//二分查找一个不重复的数字，
     }
     
 }
+int binary_search_in_resverse(int *a, int n, int value)//在旋转数组中二分查找
+{
+    int l = 0, r = n-1;//也是个闭区间内的二分查找
+    while(l<=r)
+    {
+        int m = (l + r) / 2;
+        if(a[m]==value)//第一步就判断中间位置是否就是要找的值，后面就不用考虑了
+            return m;
+        if(a[m]<a[r])//右侧有序
+        {
+            if(a[m]<value && a[r]>=value)//如果value不在右侧区间内
+                l=m+1;
+            else r = m - 1;
+        }
+        else
+        {
+            if(a[m]>value && a[l]<=value)
+                r = m - 1;
+            else
+            {
+                l = m + 1;
+            }    
+        }    
+    }
+    return -1;
+}
 int main()
 {
     cout << root(2) << endl;
     system("pause");
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << binary_search(a, 11, 9) << endl;
+
+    int b[] = {4, 5, 6, 1, 2, 3};
+    cout << binary_search_in_resverse(b, 6, 3) << endl;
     system("pause");
     return 0;
 }
